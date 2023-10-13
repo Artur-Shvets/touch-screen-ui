59e3097205ef4d0fc318ca3fba461478813b4d7c
Artur Shvets <arturshvets477@gmail.com>
2023-10-13 21:08:43 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,2 +1,36 @@
 const workSpace = document.getElementById("work-space");
 workSpace.scrollTo(1100, 1400);
+
+let isPointerDown = false;
+let downX;
+let downY;
+
+document.addEventListener(
+  "pointerdown",
+  (e) => {
+    downX = e.clientX;
+    downY = e.clientY;
+    isPointerDown = true;
+  },
+  { passive: true }
+);
+
+document.addEventListener("pointermove", (e) => {
+  if (isPointerDown) {
+    const { scrollLeft, scrollTop } = workSpace;
+    const differenceX = Math.abs(downX - e.clientX);
+    const differenceY = Math.abs(downY - e.clientY);
+
+    workSpace.scrollLeft =
+      e.clientX >= downX ? scrollLeft - differenceX : scrollLeft + differenceX;
+    workSpace.scrollTop =
+      e.clientY >= downY ? scrollTop - differenceY : scrollTop + differenceY;
+
+    downX = e.clientX;
+    downY = e.clientY;
+  }
+});
+
+document.addEventListener("pointerup", (e) => {
+  isPointerDown = false;
+});
